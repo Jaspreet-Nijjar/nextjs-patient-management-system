01206a7ff62b3339064cbf286b280c2eba161397
Jaspreet <jaspreetkn92@gmail.com>
2024-08-23 10:51:48 +0100

    creating a custom fieldType check to render an input type

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -16,25 +16,37 @@ interface CustomProps {
   control: Control<any>;
   fieldType: FormFieldType;
   name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const CustomFormField = ({
-  control,
-  fieldType,
-  name,
-  label,
-  placeholder,
-  iconSrc,
-  iconAlt,
-}: CustomProps) => {
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  return <input />;
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+
   return (
     <FormField
       control={control}
       name={name}
       render={({ field }) => (
         <FormItem>
-          {fieldType !== FormFieldType.CHECKBOX &&
-            label(<FormLabel>{label}</FormLabel>)}
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
         </FormItem>
       )}
     />
