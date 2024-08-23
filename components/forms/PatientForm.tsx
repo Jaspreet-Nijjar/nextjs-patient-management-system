520eb09180ab1a990a9eb57706f88c34c8b55c1a
Jaspreet <jaspreetkn92@gmail.com>
2024-08-23 12:06:30 +0100

    created a custom SubmitButton component

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,9 +4,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
-import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
 import CustomFormField from '../CustomFormField';
+import SubmitButton from '../SubmitButton';
+import { useState } from 'react';
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -23,6 +24,8 @@ const formSchema = z.object({
 });
 
 const PatientForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -61,7 +64,7 @@ const PatientForm = () => {
           iconAlt="email"
         />
 
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
