8088147bd61cf9c0308dc8f625ece356de48d66b
Jaspreet <jaspreetkn92@gmail.com>
2024-08-29 08:16:05 +0100

    added the user and passed as props to the registerform

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,8 +1,11 @@
 import RegisterForm from '@/components/forms/RegisterForm';
+import { getUser } from '@/lib/actions/patient.actions';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Register = () => {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
+
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
@@ -15,7 +18,7 @@ const Register = () => {
             className="mb-12 h-10 w-fit"
           />
 
-          <RegisterForm />
+          <RegisterForm user={user} />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
